a3ad3bf987e0bee9f962f736e9ba5e1cf74e9750
Dovbenko Ivan <ivan.dovbenco@esempla.com>
2022-07-24 11:04:07 +0300

    config-es-lint

--- app/src/app/areas/areas-component/areas.component.ts
+++ app/src/app/areas/areas-component/areas.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-areas-component',
-  templateUrl: './areas.component.html',
-  styleUrls: ['./areas.component.scss']
+   selector: 'bend-areas-component',
+   templateUrl: './areas.component.html',
+   styleUrls: ['./areas.component.scss'],
 })
 export class AreasComponent implements OnInit {
+   constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+   ngOnInit(): void {}
 }
--- app/src/app/things/things-component/things.component.ts
+++ app/src/app/things/things-component/things.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-things-component',
-  templateUrl: './things.component.html',
-  styleUrls: ['./things.component.scss']
+   selector: 'bend-things-component',
+   templateUrl: './things.component.html',
+   styleUrls: ['./things.component.scss'],
 })
 export class ThingsComponent implements OnInit {
+   constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+   ngOnInit(): void {}
 }
